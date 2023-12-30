9a345cf646ed7585c2858c725a37035752155c1a
Denis test <denistimakow@gmail.com>
2023-12-31 02:03:06 +0300

    Main fixed

--- main.py
+++ main.py
@@ -24,7 +24,13 @@ def google_auth():
     creds_service = ServiceAccountCredentials.from_json_keyfile_name(creds_json, scopes).authorize(httplib2.Http())
     return build('sheets', 'v4', http=creds_service)
 
-def send_message():
+@bot.message_handler(commands=['start'])
+def send_alert(message):
+    print(message)
+    bot.send_message(chat_id=message.chat.id,  text='text', protect_content=False)
+
+
+def send_notification():
     service = google_auth()
     sheet = service.spreadsheets()
     resp = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range='Текущий!A30:AG').execute()
@@ -32,13 +38,18 @@ def send_message():
     for elem in resp['values']:
         result = [item for item in [x.lower() for x in elem] if item in check_list]
         if result:
-            notification = f'Адрес: {elem[1]}, телефон {elem[3]}, литраж {elem[4]}'
-            bot.send_message(chat_id=182731743, text=notification)
+            notification = f'строка {elem[0]} Адрес: {elem[1]}, телефон {elem[3]}, литраж {elem[4]}'
+            bot.send_message(chat_id=-4097053911, text=notification)
 
-#schedule.every(15).seconds.do(send_message)
-schedule.every().day.at("12:00").do(send_message)
+schedule.every(15).seconds.do(send_notification)
+#schedule.every().day.at("12:00").do(send_message)
+#schedule.every().day.at("17:00").do(send_notification)
 
 if __name__ == '__main__':
     while True:
         schedule.run_pending()
         time.sleep(1)
+    #try:
+        #bot.polling(none_stop=True)
+    #except Exception as e:
+       # print(e)
